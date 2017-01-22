c5d4bf5c6013f4385c62709190023f8cd54ba8bb
SergeyKravchenko <kravchenko.sgv@gmail.com>
2017-01-22 19:47:51 +0300

    Реализован выбор цвета игрока перед началом игры из списка предварительно заданных цветов

--- src/game/Game.java
+++ src/game/Game.java
@@ -22,6 +22,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
